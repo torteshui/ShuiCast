84a8eea04226106f58238446c7d1958e267af43f
TorteShui <6575866+torteshui@users.noreply.github.com>
2017-09-12 15:26:18 +0200

    Synced project files.

--- trunk/shuicast/StdAfx.h
+++ trunk/shuicast/StdAfx.h
@@ -21,7 +21,7 @@
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
 
-//#ifdef _UNICODE
+#ifdef _UNICODE
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_IA64
@@ -31,7 +31,9 @@
 #else
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
-//#endif
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
 
 #ifdef HAVE_FHGAACP
 #pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.VC90.CRT' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
